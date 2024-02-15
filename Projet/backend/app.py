3f76c17176f5745d0ac7441180f2f7691be2a739
ArnaudB <118258096+Kadoby@users.noreply.github.com>
2024-02-15 10:55:46 +0000

    CSS+html

--- Projet/backend/app.py
+++ Projet/backend/app.py
@@ -5,30 +5,35 @@ app = Flask(__name__)
 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-
-
 resultats = {}
 
 @app.route("/api/tweeter", methods=["POST"])
 def tweeter():
+    return
 
-@app.route("/api/printTweet", methods=["GET"])
-def resultat(id):
+@app.route("/api/printTweet", methods=["GET"]) #Afficher les tweets
+def printTweet(id):
+    return 
 
 @app.route("/api/saveTweet", methods=["POST"])
-def resultat(id):
+def saveTweet(id):
+    return
 
 @app.route("/api/printPersonnalTweet", methods=["GET"])
-def resultat(id):
+def printPersonnalTweet(id):
+    return
 
-@app.route("/api/Retweet", methods=["POST"])
-def resultat(id):
+@app.route("/api/retweet", methods=["POST"])
+def retweet(id):
+    return
 
 @app.route("/api/printTopic", methods=["GET"])
-def resultat(id):
+def printTopic(id):
+    return
 
 @app.route("/api/printSpecificTweet", methods=["GET"])
-def resultat(id):
+def printSpecificTweet(id):
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
