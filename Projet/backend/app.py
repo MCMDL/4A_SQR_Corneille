638789338a297ce3e2f99e800077b9b5ec8edcba
MCMDL <103221729+MCMDL@users.noreply.github.com>
2024-02-25 13:37:39 +0000

    Tweeter is working and we can print all tweets

--- Projet/backend/app.py
+++ Projet/backend/app.py
@@ -21,48 +21,53 @@ resultats = {}
 
 @app.route("/api/tweeter", methods=["POST"])
 def tweeter():
-    #Ajout d'un tweet a Redis
+    #Creation de l'ID du tweet
     idTweet = r.get("idTweet")
     if idTweet==None:
         r.set("idTweet",0)
     idTweet = int(r.get("idTweet"))
     idTweet = idTweet +1
+    r.set("idTweet",idTweet)
+    #Recuperation des données puis création du stockage des tweets
     data = request.get_json()
     tweet = data["tweet"]
     username = data["username"]
-    value='{"author": username, "tweet": tweet }'
-    r.set(idTweet, value)
+    # Dictionnaire JSON pour stocker username et tweet
+    dico = json.dumps({"utilisateur": username, "message": tweet})
+    # Stockage dans Redis
+    r.set(idTweet, dico)
+    #value={"author": username, "tweet": tweet }
     #Ajout du Tweet à l'utilisateur
     userKey = "u-"+username
     r.rpush(userKey,idTweet)
-
-    # listTs = r.get(userKey)
-    # if listTs == None:
-    #     liste = [id]
-    #     listeJson  = json.loads(str(liste))
-    #     r.set(userKey, listeJson)
-    # else:
-    #     liste = json.dumps(listTs)
-    #     liste.append(id)
-    #     listeJson = json.loads(str(liste))
-    #     r.set(userKey, listeJson)
-
-
-
     #Ajouter de quoi extraire les "#" fonction findall(r"#\w+", tweet)
     print(username+"\n")
     print(tweet)
-    temp = "Tweet ajouté : " + tweet +" By :" + username
+    temp = "Tweet ajouté : " + tweet +" By :" + username +" Id is : " + str(idTweet)
     return temp
 
 
 @app.route("/api/printTweet", methods=["GET"])
-def printTweet(id):
-    value = r.get(id)
-    if value == None:
+def printTweet():
+    idTweet = int(r.get("idTweet"))
+    print(idTweet)
+    if idTweet == None:
         return "ERROR: Tweet doesn't exist"
-    tweet = value[tweet]
-    return tweet
+    temp = ""
+    for i in range (1,idTweet): 
+        tweet = str(r.get(i))
+        temp = temp + tweet +"\n"
+    return temp
+        # if tweet != None:
+        #     print(tweet)
+        #     # Désérialisation de la valeur JSON
+        #     dataFinale = json.loads(tweet)
+        #     temp = temp + dataFinale + "\n"
+        #     return temp
+        # else: 
+        #     print("ERROR: JSON value is empty")
+        #     return "ERROR: JSON value is empty"
+
     
 
 # @app.route("/api/saveTweet", methods=["POST"])
